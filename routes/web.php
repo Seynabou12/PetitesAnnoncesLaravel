6b807fb3153b9c1c572a556f4571554e721a02bf
Seynabou12 <dioneseynabou0@gmail.com>
2022-03-18 12:12:26 +0100

    Register et Authentification

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    echo 'hello tout le monde';
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-
-
-
+require __DIR__.'/auth.php';
